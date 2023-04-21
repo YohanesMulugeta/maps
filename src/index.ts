08662a9f4728a62d53a7ebde3ead153e8ffcad60
Yohanes <yohanesmulugeta21@gmail.com>
2023-04-21 22:45:12 +0300

    crate a typescript config file and set allowUmdGlobalAccess to true to work with the mapbox global script.

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,14 @@ import Company from './Company';
 const company = new Company();
 const user = new User();
 
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  accessToken:
+    'pk.eyJ1IjoieW9oYW5lczIxIiwiYSI6ImNsZ3FtbG4wcTBmeTkzZ21zdno2OW9ydDEifQ.QBjZZ40vPUm6Rjz0hgNAwQ',
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: { lng: 38.9, lat: 9.2 }, // starting position [lng, lat]
+  zoom: 4, // starting zoom
+});
+
 console.log(user);
 console.log(company);
