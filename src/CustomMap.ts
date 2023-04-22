7c34b088869889f1f06f85b7fc75359af31fd25d
Yohanes <yohanesmulugeta21@gmail.com>
2023-04-22 11:03:41 +0300

    create a customMap.ts inside the src to limit access to the map instance.

---
+++ src/CustomMap.ts
@@ -0,0 +1,23 @@
+class CustomMap {
+  private mapBox: mapboxgl.Map;
+  private marker: mapboxgl.Marker;
+
+  constructor(containerId?: string) {
+    this.mapBox = new mapboxgl.Map({
+      container: containerId ? containerId : 'map', // container ID
+      accessToken:
+        'pk.eyJ1IjoieW9oYW5lczIxIiwiYSI6ImNsZ3FtbG4wcTBmeTkzZ21zdno2OW9ydDEifQ.QBjZZ40vPUm6Rjz0hgNAwQ',
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: { lng: 38.9, lat: 9.2 }, // starting position [lng, lat]
+      zoom: 4, // starting zoom
+    });
+  }
+
+  setMarker(lngLat: mapboxgl.LngLatLike): void {
+    this.marker = new mapboxgl.Marker();
+    this.marker.setLngLat(lngLat);
+    this.marker.addTo(this.mapBox);
+  }
+}
+
+export default CustomMap;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import User from './user';
 import Company from './Company';
+import CustomMap from './CustomMap';
 
-const company = new Company();
-const user = new User();
+// const company = new Company();
+// const user = new User();
 
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  accessToken:
-    'pk.eyJ1IjoieW9oYW5lczIxIiwiYSI6ImNsZ3FtbG4wcTBmeTkzZ21zdno2OW9ydDEifQ.QBjZZ40vPUm6Rjz0hgNAwQ',
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: { lng: 38.9, lat: 9.2 }, // starting position [lng, lat]
-  zoom: 4, // starting zoom
-});
+const map = new CustomMap();
 
-console.log(user);
-console.log(company);
+map.setMarker({ lng: 38.9, lat: 9.2 });
